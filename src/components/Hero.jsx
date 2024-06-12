52c8bc263202ead5ca3dc63b1788ef3bee102eac
ShardulVanage <vsharadul@gmail.com>
2024-06-12 16:29:21 +0530

    remove

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -100,13 +100,7 @@ export default function Hero() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-blue-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
+            
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-transparent bg-clip-text bg-gradient-to-tr from-cyan-500 via-blue-500 to-blue-600 drop-shadow-lg ">
